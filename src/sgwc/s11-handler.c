504eefc7b89c8e381495c8192a2f1dcff716061b
Ryan Dimsey <ryan@omnitouch.com.au>
2023-08-22 15:34:53 +1000

    Quick fix - SGWC remove only the session specified in the CSR

--- src/sgwc/s11-handler.c
+++ src/sgwc/s11-handler.c
@@ -264,10 +264,8 @@ void sgwc_s11_handle_create_session_request(
         
         /* "All functionality performed by the SGW-U is controlled from the SGW-C"
          * We need to also remove the session resources in the SGW-U */
-        ogs_list_for_each(&sgwc_ue->sess_list, sess) {
-            ogs_expect(OGS_OK ==
-                sgwc_pfcp_send_session_deletion_request(sess, NULL, NULL));
-        }
+        ogs_expect(OGS_OK ==
+            sgwc_pfcp_send_session_deletion_request(sess, NULL, NULL));
     }
     sess = sgwc_sess_add(sgwc_ue, apn);
     ogs_assert(sess);
